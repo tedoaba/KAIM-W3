db76109d7c24fc2cf6bd4cc481c6fedecd86b451
tedoaba <tedoaba@gmail.com>
2024-09-17 22:13:09 +0300

    Evaluation

---
+++ src/evaluation.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import mean_squared_error, r2_score
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    return mse, r2
