9858117b2b743f78ceb35f7dd19d5e6ea4aa0c45
tedoaba <tedoaba@gmail.com>
2024-09-17 15:41:54 +0300

    data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,3 +9,15 @@ def preprocess_data(df):
     df.fillna('Unknown', inplace=True)
     df['TransactionMonth'] = pd.to_datetime(df['TransactionMonth'], errors='coerce')
     return df
+def display_basic_info(df):
+    print("Data Overview")
+    print(df.head())
+    print(f"Shape: {df.shape}")
+    df.info()
+    print(df.columns)
+
+def print_unique_values(df):
+    for column in df.columns:
+        print(f"Unique values for column '{column}':")
+        print(df[column].unique())
+        print() 
