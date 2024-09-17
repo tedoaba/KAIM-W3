0059bcae6a83de3cce00308eed4b147f7b692ba0
tedoaba <tedoaba@gmail.com>
2024-09-17 15:53:41 +0300

    Main

--- scripts/main.py
+++ scripts/main.py
@@ -3,27 +3,14 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import pandas as pd
-from data_loader import load_data, preprocess_data, display_basic_info, print_unique_values
-from eda import summarize_data, check_missing_values, correlation_analysis, handle_missing_values, get_numerical_columns, correlation_analysis, data_compression
-from visualization import plot_histogram, plot_scatter, plot_missing_values, plot_histograms, plot_categorical_bars, plot_correlation_matrix, plot_postalcode_premium, plot_premium_vs_claims
+from data_loader import load_data, display_basic_info, print_unique_values
+from eda import check_missing_values,  handle_missing_values, get_numerical_columns, correlation_analysis, data_compression
+from visualization import plot_missing_values, plot_histograms, plot_categorical_bars, plot_correlation_matrix, plot_postalcode_premium, plot_premium_vs_claims
 
 def main():
     # Data Collection
     file_path = '../data/cleaned_insurance_data.csv'
     df = load_data(file_path)
-    
-    #df = preprocess_data(df)
-    
-    # EDA tasks
-    #summary = summarize_data(df)
-    #missing = check_missing_values(df)
-    #correlation = correlation_analysis(df)
-    
-    # Visualization tasks
-    #plot_histogram(df, 'TotalPremium')
-    #plot_scatter(df, 'TotalPremium', 'TotalClaims', 'PostalCode')
-    
-    #print("EDA complete!")
     
     # Display basic information about the dataset
     display_basic_info(df)
@@ -47,6 +34,8 @@ def main():
     # Visualizations
     plot_postalcode_premium(df)
     plot_premium_vs_claims(df)
+    
+    print("EDA complete!")
     
 if __name__ == '__main__':
     main()
--- src/data_loader.py
+++ src/data_loader.py
@@ -4,11 +4,6 @@ def load_data(file_path):
     """Load the dataset from a CSV file."""
     return pd.read_csv(file_path, delimiter='|')
 
-def preprocess_data(df):
-    """Perform basic preprocessing tasks such as handling missing values."""
-    df.fillna('Unknown', inplace=True)
-    df['TransactionMonth'] = pd.to_datetime(df['TransactionMonth'], errors='coerce')
-    return df
 def display_basic_info(df):
     print("Data Overview")
     print(df.head())
